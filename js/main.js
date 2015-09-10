1022634e514af095a04a0c14bdb15d960ca6848d
devakdo <you@example.com>
2015-09-10 13:56:45 +0530

    map,schedule and about us updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(function($) {'use strict',
 	
 	//Countdown js
 	 $("#countdown").countdown({
-			date: "10 july 2017 12:00:00",
+			date: "19 septemper 2015 10:00:00",
 			format: "on"
 		},
 		
